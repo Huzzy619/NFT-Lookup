566815148a0afc57df2cefc8ff79c70f74819c3a
Huzzy619 <blazingkrane@gmail.com>
2022-09-05 17:52:17 +0100

    cloudinary

--- NFT_LOOKOUT/dev.py
+++ NFT_LOOKOUT/dev.py
@@ -1,4 +1,5 @@
 from .settings import *
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure--ojc&jrm^1r94qlu1a*@1pv4x$_8evky+u)xfsm(f$8-0+j)p^"
 
@@ -18,9 +19,9 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'new_4', 
+        'NAME': config('NAME'), 
         'USER': 'postgres', 
-        'PASSWORD': '0509',
+        'PASSWORD': config('PASSWORD'),
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
--- NFT_LOOKOUT/settings.py
+++ NFT_LOOKOUT/settings.py
@@ -33,7 +33,10 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.staticfiles',
+    
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
