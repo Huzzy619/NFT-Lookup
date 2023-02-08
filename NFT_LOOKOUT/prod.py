ce70fc8025ab553d7b4adb7da3f8e83d6d3b1739
Huzzy619 <blazingkrane@gmail.com>
2023-02-08 03:20:14 +0100

    ch

--- NFT_LOOKOUT/prod.py
+++ NFT_LOOKOUT/prod.py
@@ -3,7 +3,7 @@ import os
 # import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
