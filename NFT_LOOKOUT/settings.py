8377143f32dd652e27841d230f8496a31f0c8138
Huzzy619 <blazingkrane@gmail.com>
2023-02-08 18:35:06 +0100

    changed database

--- NFT_LOOKOUT/settings.py
+++ NFT_LOOKOUT/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "NFT_LOOKOUT.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': 'lookdb.sqlite3',
     }
 }
 
