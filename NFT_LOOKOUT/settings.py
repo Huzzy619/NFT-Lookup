836d161189df5c703f2b7bb765ac8399df433a0d
Huzzy619 <blazingkrane@gmail.com>
2023-03-02 01:37:17 +0100

    redeploy

--- NFT_LOOKOUT/settings.py
+++ NFT_LOOKOUT/settings.py
@@ -166,7 +166,7 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 
-# Default primary key field type
+# Default primary key field type.
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
