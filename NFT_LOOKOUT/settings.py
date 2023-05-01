fe18c7a6c209f67d044b0fce9350868fbf43bbb5
Huzzy619 <blazingkrane@gmail.com>
2023-05-01 23:39:12 +0100

    redeploy May

--- NFT_LOOKOUT/settings.py
+++ NFT_LOOKOUT/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'core',
     'nft_calendar', 
     'rarenfts',
-    'jazzmin',
+    # 'jazzmin',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
